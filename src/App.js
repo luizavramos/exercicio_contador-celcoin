c4de513deb839fd2bd70424f324472946443549c
luizavramos <luizavramos30@gmail.com>
2022-10-27 22:40:14 -0300

    Exercicio contador

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function sum() {
+    setCounter(counter + 1);
+  }
+
+  function sub() {
+    setCounter(counter - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <h1>{counter}</h1>
+    <button onClick={sum}>+</button>
+    <button onClick={sub}>-</button>
+  </div>
   );
 }
 
